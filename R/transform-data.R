3673b66ff0cd061a4dfbbb6b0b30a47df2cb9b90
auzaheta <alvaro.uzaheta@gess.ethz.ch>
2025-11-05 17:02:57 +0100

    Refactor `write_json()`

--- R/transform-data.R
+++ R/transform-data.R
@@ -68,9 +68,10 @@ make_cox_model_data <- function(data, sub_model = c("rate", "choice")) {
 #' }
 #' @noRd
 make_df_cstr <- function(
-    processed_data,
-    extended_formula,
-    names_effects) {
+  processed_data,
+  extended_formula,
+  names_effects
+) {
   expanded_df <- with(processed_data, {
     n_events <- length(sender)
 
@@ -81,8 +82,7 @@ make_df_cstr <- function(
       ),
       data.frame(
         event = rep(seq.int(n_events), n_candidates),
-        selected = sequence(n_candidates) ==
-          rep(selected, n_candidates),
+        selected = sequence(n_candidates) == rep(selected, n_candidates),
         sender = rep(sender, n_candidates)
       )
     )
@@ -106,7 +106,8 @@ make_df_cstr <- function(
     cli::cli_warn(c(
       cli_text(
         "There is a mismatch between the number of receivers ",
-        "before and after constraint."),
+        "before and after constraint."
+      ),
       "i" = "receivers before: {n_origin}, receivers after: {n_selected}"
     ))
   }
@@ -156,9 +157,11 @@ make_df_from_stan <- function(data_stan, sub_model = c("rate", "choice")) {
 # Helper function to process a submodel
 sample_set <- function(
   data_stan,
-  model, sub_model,
+  model,
+  sub_model,
   method_set,
-  fraction_set, sample_size_set = NULL,
+  fraction_set,
+  sample_size_set = NULL,
   sample_events = NULL
 ) {
   data_sample <- list()
@@ -208,15 +211,18 @@ sample_set <- function(
               N = nrow(non_selected_rows),
               fraction = fraction_set,
               sample_size = sample_size_set
-            ))
+            )
+          )
           non_selected_rows <- non_selected_rows[sample$sample, ]
           non_selected_rows[, "offset_sample"] <- 1 / sample$sel_prob
         }
-        if (nrow(selected_row) == 0) selected_row <- NULL
+        if (nrow(selected_row) == 0) {
+          selected_row <- NULL
+        }
         rbind(selected_row, non_selected_rows)
       }
     ) |>
-    Reduce(f = rbind, x = _)
+      Reduce(f = rbind, x = _)
   }
   data_sample[[glue("offset_set_{sub_model}")]] <- sample_df$offset_sample
 
@@ -245,313 +251,4 @@ sample_set <- function(
   }
 
   data_sample
-}
-
-#' Write model data to a JSON file
-#'
-#' When the data is too large, the JSON string can be too long and cause
-#' and error when writing the data to a file
-#' (R character strings are limited to 2^31-1 bytes).
-#' In that case, the JSON string is divided into chunks.
-#' @param x Data object
-#' @param file_name File name to write the JSON string to,
-#'   needs the .json extension.
-#' @param n_chunks Number of chunks to divide the JSON string to avoid
-#'   the error.
-#' @param grain_size Number of events to process in each thread when 
-#'   using within chain parallelization with Map-Reduce.
-#' @return NULL
-#' @export
-write_json <- function(x, file_name, n_chunks = 10, grain_size = 5) {
-  model <- attr(x, "model")
-  sub_model <- attr(x, "sub_model")
-  has_sample <- attr(x, "sample")
-  scale <- attr(x, "scale")
-
-  # object with information for postprocessing
-  data_gathered <- x[!grepl("data_stan", names(x))]
-  data_gathered$json_file <- file
-
-  # Preserve class and attributes from the original data object
-  class(data_gathered) <- class(x)
-  attr(data_gathered, "model") <- model
-  attr(data_gathered, "subModel") <- sub_model
-  attr(data_gathered, "sample") <- has_sample
-  attr(data_gathered, "scale") <- scale
-  attr(data_gathered, "json_file") <- TRUE
-
-  # prepare data for JSON and calculate approx. size  
-  data_stan <- x$data_stan
-  n_size <- data_stan[[glue("N_{sub_model}")]]
-  t_size <- data_stan[[glue("T_{sub_model}")]]
-  p_size <- data_stan[[glue("P_{sub_model}")]]
-  q_size <- data_stan[[glue("Q_{sub_model}")]]
-  x_name <- glue("X_{sub_model}")
-  z_name <- glue("Z_{sub_model}")
-  has_z <- q_size > 0
-  has_interaction <- !is.null(data_stan[["interaction"]])
-  is_integer_x <- rlang::is_integerish(data_stan[[x_name]])
-
-  approx_size_x <- approx_nchar_matrix(
-    n_size, p_size,
-    max_val = max(data_stan[[x_name]]), is_integer = is_integer_x
-  )
-
-  if (has_z) {
-    is_integer_z <- rlang::is_integerish(data_stan[[z_name]]) 
-    approx_size_z <- approx_nchar_matrix(
-      n_size, q_size,
-      max_val = max(data_stan[[z_name]]), is_integer = is_integer_z
-    )
-  } else {
-    approx_size_z <- 0
-    data_stan[[z_name]] <- NULL
-  }
-  if (has_interaction) {
-    approx_size_interaction <- approx_nchar_vector(t_size, 100)
-  } else {
-    approx_size_interaction <- 0
-  }
-  if (sub_model == "rate") {
-    mode(data_stan[["is_dependent"]]) <- "integer"
-    approx_size_is_dependent <- approx_nchar_vector(t_size, 1)
-    is_integer_timespan <- rlang::is_integerish(data_stan[["timespan"]])
-    approx_size_timespan <- approx_nchar_vector(
-      t_size,
-      max(data_stan[["timespan"]]),
-      is_integer = is_integer_timespan
-    )
-    approx_size_rate <- approx_size_is_dependent + approx_size_timespan
-  } else {
-    approx_size_rate <- 0
-  }
-  approx_max_size <- max(approx_size_x, approx_size_z)
-  aprox_size  <- approx_nchar_vector(t_size, n_size)
-  total_size <- aprox_size * 3 + approx_size_x + approx_size_z +
-    approx_size_rate + approx_size_interaction + 200
-  if (total_size < (2^31 - 1)) {
-    data_stan[["grain_size"]] <- grain_size
-    cmdstanr::write_stan_json(
-      data = data_stan,
-      file = file_name
-    )
-    return(data_gathered)
-  }
-
-  if (approx_max_size > (2^31 - 1)) {
-    row_partition <- parallel::splitIndices(n_size, n_chunks)
-    x_text <- c(
-      glue("\"X_{sub_model}\": ["),
-      vapply(
-        row_partition,
-        create_json_chunk_matrix,
-        character(1),
-        matrix = data_stan[[x_name]]
-      ),
-      "],"
-    )
-    stringr::str_sub(x_text[n_chunks + 1], -1) <- ""
-    if (has_z) {
-      z_text <- c(
-        glue("\"Z_{sub_model}\": ["),
-        vapply(
-          row_partition,
-          create_json_chunk_matrix,
-          character(1),
-        matrix = data_stan[[z_name]]
-      ),
-      "],"
-    )
-    stringr::str_sub(z_text[n_chunks + 1], -1) <- ""
-    }
-  } else {
-    x_text <- c(
-      glue("\"X_{sub_model}\":"),
-      toJSON(data_stan[[x_name]], pretty = TRUE, digits = NA),
-      ","
-    )
-    if (has_z) {
-      z_text <- c(
-        glue("\"Z_{sub_model}\":"),
-        toJSON(data_stan[[z_name]], pretty = TRUE, digits = NA),
-        ","
-      )
-    }
-  }
-
-  if (aprox_size > (2^31 - 1)) {
-    vector_partition <- parallel::splitIndices(t_size, n_chunks)
-    start_text <- c(
-      glue("\"start_{sub_model}\": ["),
-      vapply(
-        vector_partition,
-        create_json_chunk_vector,
-        character(1),
-        vector = data_stan[[glue("start_{sub_model}")]]
-      ),
-      "],"
-    )
-    stringr::str_sub(start_text[n_chunks + 1], -1) <- ""
-    end_text <- c(
-      glue("\"end_{sub_model}\": ["),
-      vapply(
-        vector_partition,
-        create_json_chunk_vector,
-        character(1),
-        vector = data_stan[[glue("end_{sub_model}")]]
-      ),
-      "],"
-    )
-    stringr::str_sub(end_text[n_chunks + 1], -1) <- ""
-    chose_text <- c(
-      glue("\"chose_{sub_model}\": ["),
-      vapply(
-        vector_partition,
-        create_json_chunk_vector,
-        character(1),
-        vector = data_stan[[glue("chose_{sub_model}")]]
-      ),
-      "],"
-    )
-    stringr::str_sub(chose_text[n_chunks + 1], -1) <- ""
-    sender_text <- c(
-      "\"sender\": [",
-      vapply(
-        row_partition,
-        create_json_chunk_vector,
-        character(1),
-      vector = data_stan[["sender"]]
-    ),
-    "],"
-    )
-    stringr::str_sub(sender_text[n_chunks + 1], -1) <- ""
-    # start_group_text <- c(
-    #   "\"start_group\": [",
-    #   vapply(
-    #     row_partition,
-    #     create_json_chunk_vector,
-    #     character(1),
-    #   vector = data_stan[["start_group"]]
-    # ),
-    # "],"
-    # )
-    # stringr::str_sub(start_group_text[n_chunks + 1], -1) <- ""
-    if (has_interaction) {
-      interaction_text <- c(
-        "\"interaction\": [",
-        vapply(
-          vector_partition,
-          create_json_chunk_vector,
-          character(1),
-          vector = data_stan[["interaction"]]
-        ),
-        ","
-      )
-      stringr::str_sub(interaction_text[n_chunks + 1], -1) <- ""
-    }
-    if (sub_model == "rate") {
-      timespan_text <- c(
-        "\"timespan\": [",
-        vapply(
-          vector_partition,
-          create_json_chunk_vector,
-          character(1),
-          vector = data_stan[["timespan"]]
-        ),
-        "],"
-      )
-      stringr::str_sub(timespan_text[n_chunks + 1], -1) <- ""
-      is_dependent_text <- c(
-        "\"is_dependent\": [",
-        vapply(
-          vector_partition,
-          create_json_chunk_vector,
-          character(1),
-          vector = data_stan[["is_dependent"]]
-        ),
-        "],"
-      )
-      stringr::str_sub(is_dependent_text[n_chunks + 1], -1) <- ""
-    }
-  } else {
-    start_text <- c(
-      glue("\"start_{sub_model}\":"),
-      toJSON(data_stan[[glue("start_{sub_model}")]], pretty = TRUE),
-      ","
-    )
-    end_text <- c(
-      glue("\"end_{sub_model}\":"),
-      toJSON(data_stan[[glue("end_{sub_model}")]], pretty = TRUE),
-      ","
-    )
-    chose_text <- c(
-      glue("\"chose_{sub_model}\":"),
-      toJSON(data_stan[[glue("chose_{sub_model}")]], pretty = TRUE),
-      ","
-    )
-    sender_text <- c(
-      "\"sender\":",
-      toJSON(data_stan[["sender"]], pretty = TRUE),
-      ","
-    )
-    # start_group_text <- c(
-    #   "\"start_group\":",
-    #   toJSON(data_stan[["start_group"]], pretty = TRUE),
-    #   ","
-    # )
-    if (has_interaction) {
-      interaction_text <- c(
-        "\"interaction\":",
-        toJSON(data_stan[["interaction"]], pretty = TRUE),
-        ","
-      )
-    }
-    if (sub_model == "rate") {
-      timespan_text <- c(
-        "\"timespan\":",
-        toJSON(data_stan[["timespan"]], pretty = TRUE),
-        ","
-      )
-      is_dependent_text <- c(
-        "\"is_dependent\":",
-        toJSON(data_stan[["is_dependent"]], pretty = TRUE),
-        ","
-      )
-    }
-  }
-
-  keep_dttxt <- c(
-    glue("{data}_{sub_model}", data = c("N", "T", "P", "Q")),
-    c("A", if (has_interaction) "C" else NULL)
-  )
-  data_text <- transform_json(
-    c(data_stan[keep_dttxt], list(grain_size = grain_size)),
-    first_position = 3,
-    first_replace = "",
-    last_replace = "}"
-  )
-
-  # Write the data to a json file
-  conn <- file(file_name, open = "wb")
-  writeLines("{", conn, useBytes = TRUE)
-  writeLines(start_text, conn)
-  writeLines(end_text, conn)
-  writeLines(chose_text, conn)
-  writeLines(sender_text, conn)
-  # writeLines(start_group_text, conn)
-  writeLines(x_text, conn)
-  if (has_z) {
-    writeLines(z_text, conn)
-  }
-  if (has_interaction) {
-    writeLines(interaction_text, conn)
-  }
-  if (sub_model == "rate") {
-    writeLines(timespan_text, conn)
-    writeLines(is_dependent_text, conn)
-  }
-  writeLines(data_text, conn)
-  close(conn)
-
-  data_gathered
 }
